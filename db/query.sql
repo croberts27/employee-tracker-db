1d3434bd72a63978e5c1af39a5da8aaed7d7dd9d
Calvin Roberts <you@example.com>
2023-06-21 16:33:46 -0600

    updated seeds.sql to resolve error

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,3 @@
 SELECT * FROM department;
-SELECT * FROM role;
+SELECT * FROM roles;
 SELECT * FROM employee;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,39 +1,22 @@
 -- Populate data into department table
-INSERT INTO department (name)
-VALUE ("Sales");
-INSERT INTO department (name)
-VALUE ("Marketing");
-INSERT INTO department (name)
-VALUE ("Finance");
-INSERT INTO department (name)
-VALUE ("HR");
+INSERT INTO department (name) VALUES ("Sales");
+INSERT INTO department (name) VALUES ("Marketing");
+INSERT INTO department (name) VALUES ("Finance");
+INSERT INTO department (name) VALUES ("HR");
 
 -- Populate data into roles table
-INSERT INTO role (title, salary, department_id)
-VALUE ('Manager', 100000, 1);
-INSERT INTO role (title, salary, department_id)
-VALUE ('Sales Representative', 90000, 2);
-INSERT INTO role (title, salary, department_id)
-VALUE ('Marketing Coordinator', 30000, 3);
-INSERT INTO role (title, salary, department_id)
-VALUE ("Sales Lead", 100000, 1);
-INSERT INTO role (title, salary, department_id)
-VALUE ('Financial Analyst', 45000, 4);
-INSERT INTO role (title, salary, department_id)
-VALUE ('HR Specialist', 80000, 5);
+INSERT INTO roles (title, salary, department_id) VALUES ('Manager', 100000, 1);
+INSERT INTO roles (title, salary, department_id) VALUES ('Sales Representative', 90000, 1);
+INSERT INTO roles (title, salary, department_id) VALUES ('Marketing Coordinator', 30000, 2);
+INSERT INTO roles (title, salary, department_id) VALUES ("Sales Lead", 100000, 1);
+INSERT INTO roles (title, salary, department_id) VALUES ('Financial Analyst', 45000, 3);
+INSERT INTO roles (title, salary, department_id) VALUES ('HR Specialist', 80000, 4);
 
 -- Populate data into employees table
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ('John', 'Doe', null, 1);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ('Jane', 'Doe', null, 2);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ('Michael', 'Douglas', 1, 3);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ('Martin', 'Sheen', 2, 4);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ('Jack', 'Kirby', null, 5);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ('Stan', 'Lee', 3, 6);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ('Balthazar', 'Jones', 4, 7);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('John', 'Doe', null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Jane', 'Doe', null, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Michael', 'Douglas', 1, 3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Martin', 'Sheen', 2, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Jack', 'Kirby', null, 5);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Stan', 'Lee', 3, 6);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Balthazar', 'Jones', 4, 7);
